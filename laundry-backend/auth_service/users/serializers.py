5b1276ddb4030d88d8b851939856f1a929f8801f
Gurumurugan Arumugam <gurumurugan.arumugam@impigertech.com>
2025-06-16 15:23:48 +0530

    Created auth_service for backend Django project setup

---
+++ laundry-backend/auth_service/users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
